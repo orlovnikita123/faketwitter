40b76d405740bf6e849cea271878ed777b6d06f8
Nikita <nick-orlov2008@yandex.ru>
2017-01-18 10:00:25 +0000

    c9

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  
+  match '/', to: 'static_pages#home', via: 'get'
+  match '/help',to: 'static_pages#help', via: 'get'
+  match '/about',to: 'static_pages#about', via: 'get'
+
+  resources :users
+  resources :microposts
+  
+  root to: 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

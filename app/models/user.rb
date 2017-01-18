354e0dfcb554e58500bdf9efecd9fa605412eec4
Nikita <nick-orlov2008@yandex.ru>
2017-01-18 10:20:29 +0000

    c9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   has_many :microposts
+  
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_uniqueness_of :email, case_sensitive: false
+  validates_format_of :email, with: VALID_EMAIL_REGEX
 end
+
